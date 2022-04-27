1fbc0eca2482724c6411ffe2c76dfa82182a3f0d
Yruama_Lairba <yruama_lairba@hotmail.com>
2022-04-27 02:44:39 +0200

    fix, missing derive on marker

--- src/lib.rs
+++ src/lib.rs
@@ -28,9 +28,11 @@ use self::pac::spi1::{i2spr, sr};
 //use crate::pac::spi1::i2scfgr::I2SCFG_A;
 
 /// Marker, indicated master mode.
+#[derive(Debug, Clone, Copy)]
 pub struct Master;
 
 /// Marker, indicate slave mode.
+#[derive(Debug, Clone, Copy)]
 pub struct Slave;
 
 /// The channel associated with a sample
